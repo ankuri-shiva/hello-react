db9b7e26d4d339679abd6f4200e559a3681ed65f
ankuri-shiva <ankurishiva@gmail.com>
2023-12-04 11:29:36 +0530

    commit2

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-// const heading = React.createElement("h1", 
-// {id : "heading"}, 
-// "Hello World from React");
+import React from "react"
+import ReactDOM  from "react-dom/client";
 
 const parent = React.createElement("div", {id : "parent"} , [
     React.createElement("div", {id : "child"}, [
-        React.createElement("h1", {id : "heading"}, "Iam an h1 tag"),
-    React.createElement("h12", {id : "heading1"}, "Iam an h2 tag")
+        React.createElement("h1", {id : "heading"}, "This is react"),
+    React.createElement("h2", {id : "heading1"}, "Iam an h2 tag")
     ]),
     React.createElement("div", {id : "child1"}, [
         React.createElement("h1", {}, "Iam an h1 tag"),
     React.createElement("h2", {}, "Iam an h2 tag")
     ])
-]) // React.createElement() creates an object
+]);
 
-console.log(parent) // object
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // reactDom is responsible for creating root or assigning root inside react
+const root = ReactDOM.createRoot(document.getElementById("root")); 
 
-root.render(parent); // it converts the object to html element which is being understood by web browesers
+root.render(parent); 
